75432f6bc72a62eb027033663df0fa32579c0e78
kiart-tantasi <petch_rwb@hotmail.com>
2022-04-07 06:02:13 +0700

    added some interfaces to MyLambda.class

--- src/MyLambda.java
+++ src/MyLambda.java
@@ -9,25 +9,88 @@ public class MyLambda {
         animalArrayList.add("Cat");
         animalArrayList.add("Pig");
 
+        // Lambda with forEach
         animalArrayList.forEach(x -> System.out.println(".forEach(): " + x));
 
         System.out.println("\n");
 
+        // Lambda with forEach
         animalArrayList.forEach(x -> {
             System.out.println(".forEach() 2: " + x);
         });
 
         System.out.println("\n");
 
-        Consumer<String> consumerLambdaMethod = (x) -> System.out.println("Consumer: " + x);
+        // insert method into forEach
+        Consumer<String> consumerLambdaMethod = x -> System.out.println(".forEach() 3: " + x);
         animalArrayList.forEach(consumerLambdaMethod);
 
         System.out.println("\n");
-        MyFunction sayHi = (str) -> System.out.println("Hi ! " + str + " How are you ?");
-        sayHi.runFunction("David.");
+
+        // insert method into interface's method
+        MyFunction sayHi = name -> "Hi ! " + name;
+        String hiDavid = sayHi.runFunction("David.");
+        System.out.println(hiDavid);
+
+        System.out.println("\n");
+
+        // ----- Lambda Techniques ----- //
+        // 1
+        doLambda(() -> System.out.println("What's up !"));
+
+        System.out.println("\n");
+
+        // 2
+        Printable printMethod = () -> System.out.println("Printing Something");
+        doLambda(printMethod);
+
+        System.out.println("\n");
+
+        // 3
+        PrintableTakesString printableTakesString = (str) -> System.out.println(str + ": I am taking a string parameter.");
+        doLambdaWithParam(printableTakesString);
+
+        System.out.println("\n");
+
+        // 4 (Last Example)
+        StringReturnable stringReturnable = (String string) -> string;
+        String strReturned = lambdaToReturnString(stringReturnable);
+        System.out.println(strReturned);
+
+
+    }
+
+    static void doLambda(Printable sout) { // got to implement interface
+        sout.print();
+    }
+
+    static void doLambdaWithParam(PrintableTakesString sout) { // got to implement interface
+        sout.printWithString("Anna");
+    }
+
+    static String lambdaToReturnString(StringReturnable fn) {
+        return fn.returnString("This is string returned.");
     }
 }
 
+// interfaces to implement
+
+@FunctionalInterface
 interface MyFunction {
-    void runFunction(String str);
+    String runFunction(String name);
+}
+
+@FunctionalInterface
+interface Printable {
+    void print();
+}
+
+@FunctionalInterface
+interface PrintableTakesString {
+    void printWithString(String str);
+}
+
+@FunctionalInterface
+interface StringReturnable {
+    String returnString(String str);
 }
