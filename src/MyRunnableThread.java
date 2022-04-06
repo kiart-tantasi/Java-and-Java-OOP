a250d6fc9b5c7391bbc9db05aac0f52531bc8335
kiart-tantasi <petch_rwb@hotmail.com>
2022-04-07 02:31:47 +0700

    added thread.join()

--- src/MyRunnableThread.java
+++ src/MyRunnableThread.java
@@ -9,24 +9,40 @@ public class MyRunnableThread implements Runnable {
     public void run() {
         for (int i = 0; i <= rounds; i++) {
             System.out.println("Until " + rounds + ": " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args) {
         MyRunnableThread runnableThread = new MyRunnableThread(5);
         MyRunnableThread runnableThreadTwo = new MyRunnableThread(3);
-        Thread thread = new Thread(runnableThread);
+        Thread threadOne = new Thread(runnableThread);
         Thread threadTwo = new Thread(runnableThreadTwo);
 //        thread.start();
 //        threadTwo.start();
 
         System.out.println("Run one thread by one time\n");
 
-        thread.start();
-        while (thread.isAlive()) {}
+        threadOne.start();
+//        while (thread.isAlive()) {}
+        try {
+            threadOne.join();
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
         System.out.println("thread one is done.\n");
+
         threadTwo.start();
-        while (threadTwo.isAlive()) {}
+//        while (threadTwo.isAlive()) {}
+        try {
+            threadTwo.join();
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
         System.out.println("thread two is done.");
     }
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -1,35 +1,66 @@
 public class MyThread extends Thread {
 
+    public Integer threadNumber;
     public Integer rounds;
 
-    public MyThread(Integer rounds) {
+    public MyThread(Integer threadNumber, Integer rounds) {
+        this.threadNumber = threadNumber;
         this.rounds = rounds;
     }
 
     public void run() {
         for (int i = 0; i <= rounds; i++) {
-            System.out.println("Until " + rounds + ": " + i);
+            System.out.println("Thread No." + this.threadNumber + ": " + i);
+
+            // make it slower (500 milli for each loop)
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
     public static void main(String[] args) {
 
         // Thread
-//        MyThread myThread = new MyThread(7);
-//        MyThread myThreadTwo = new MyThread(8);
-////        Thread threadTest = new Thread(myThread);
-////        threadTest.start();
+//        MyThread myThread = new MyThread(1,7);
+//        MyThread myThreadTwo = new MyThread(2,8);
 //        myThread.start();
+//        myThread.join();
+//        System.out.println("\n");
 //        myThreadTwo.start();
+//        myThreadTwo.join();
+//        System.out.println("\n");
 
-        // isAlive
-        MyThread threadToFive = new MyThread(5);
-        MyThread threadToThree = new MyThread(3);
-        threadToFive.start();
-        while (threadToFive.isAlive()) {}
-        System.out.println("FIRST THREAD DONE");
-        threadToThree.start();
-        while (threadToThree.isAlive()) {}
-        System.out.println("ALL THREADS DONE !");
+        // new Thread(myThread)
+//        Thread threadTest = new Thread(myThread);
+//        threadTest.start();
+//        threadTest.join();
+
+        MyThread threadOne = new MyThread(1,4);
+        MyThread threadTwo = new MyThread(2,3);
+
+        // THREAD 1
+        threadOne.start();
+//        while (threadToFive.isAlive()) { }
+        try {
+            threadOne.join();
+        } catch(InterruptedException ex) {
+            System.out.println(ex);
+        }
+        System.out.println("FIRST THREAD DONE\n");
+
+        // THREAD 2
+        threadTwo.start();
+//        while (threadToThree.isAlive()) { }
+        try {
+            threadTwo.join();
+        } catch(InterruptedException ex) {
+            System.out.println(ex);
+        }
+        System.out.println("SECOND THREAD DONE");
+
     }
 }
