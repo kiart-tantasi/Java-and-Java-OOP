24ade1bf4621e496fdfff10fe1026f03ddcf043d
kiart-tantasi <petch_rwb@hotmail.com>
2024-03-02 18:06:53 +0700

    refactor SocketDemo.java and change port 3000 to 8080

--- com/example/networking/SocketDemo.java
+++ com/example/networking/SocketDemo.java
@@ -6,7 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-// NOTE: Run mock api first
+// NOTE: Before running this file, you need to start mock-api first
+// (at root)
+// ```
+// cd mock-api
+// npm install
+// node index.js
+// ```
 
 public class SocketDemo {
     public static void main(String[] argv) {
@@ -14,21 +20,23 @@ public class SocketDemo {
     }
 
     protected void run(String hostName) {
-        try (Socket sock = new Socket(hostName, 3000);) {
+        System.out.println("Starting...");
+        try (Socket sock = new Socket(hostName, 8080);) {
+            System.out.println("Socket is connected...");
+            // Create output-stream and connect to the socket to send HTTP request
             PrintWriter os = new PrintWriter(sock.getOutputStream(), true);
-            // Send the HTTP GET request
+            // Send HTTP request
             os.println("GET / HTTP/1.1");
             os.println();
-
-            // Read input
+            // Read input-stream
             BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println("[LINE] " + line);
             }
-
             // Close the socket (this will also close the output stream)
             sock.close();
+            System.out.println("Socket is closed...");
         } catch (IOException e) {
             System.err.println(e);
         }
